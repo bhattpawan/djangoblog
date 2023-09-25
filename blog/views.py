cabd53563898ad92fb9d73b786c5498221acfce6
bhattpawan <bhattpawan1997@gmail.com>
2023-09-25 22:45:26 +0530

    Added views and templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        "author": "Pawan Bhatt",
+        "title": "Blog Post 1",
+        "content": "First Post Content",
+        "date_posted": "Sept 25,2023",
+    },
+    {
+        "author": "John Doe",
+        "title": "Blog Post 2",
+        "content": "Second Post Content",
+        "date_posted": "Aug 20,2023",
+    },
+]
+
+
 # Create your views here.
+def home(request):
+    context = {"posts": posts, "title": "Home"}
+    return render(request, template_name="blog/home.html", context=context)
+
+
+def about(request):
+    context = {"title": "About"}
+    return render(request, template_name="blog/about.html", context=context)
