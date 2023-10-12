bc97627f15796e0d5d99183f8ef3bd666f338d1b
bhattpawan <bhattpawan1997@gmail.com>
2023-10-12 09:44:31 +0530

    Post Detail page

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
+from typing import Any
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
-# Create your views here.
 def home(request):
     posts = Post.objects.all()
     context = {
@@ -11,6 +12,29 @@ def home(request):
         "sidebar_posts": posts.order_by("-last_modified"),
     }
     return render(request, template_name="blog/home.html", context=context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/home.html"
+    context_object_name = "posts"
+    ordering = ["-date_posted"]
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["sidebar_posts"] = Post.objects.all().order_by("-last_modified")
+        context["title"] = "Home"
+        return context
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["sidebar_posts"] = Post.objects.all().order_by("-last_modified")
+        context["title"] = "Home"
+        return context
 
 
 def about(request):
