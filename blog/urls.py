86253ca523823a62945a0496443496a9bc8a1ff2
bhattpawan <bhattpawan1997@gmail.com>
2023-10-13 09:53:32 +0530

    Added create and update templat and viewse for posts

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.PostListView.as_view(), name="blog-home"),
-    path("post/<str:pk>/", views.PostDetailView.as_view(), name="post-detail"),
+    path("post/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
+    path("post/new/", views.PostCreateView.as_view(), name="post-create"),
+    path("post/update/<int:pk>/", views.PostUpdateView.as_view(), name="post-update"),
     path("about/", views.about, name="blog-about"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,13 @@
-from typing import Any
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, redirect
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
-
-
-def home(request):
-    posts = Post.objects.all()
-    context = {
-        "posts": posts,
-        "title": "Home",
-        "sidebar_posts": posts.order_by("-last_modified"),
-    }
-    return render(request, template_name="blog/home.html", context=context)
 
 
 class PostListView(ListView):
@@ -20,9 +16,9 @@ class PostListView(ListView):
     context_object_name = "posts"
     ordering = ["-date_posted"]
 
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["sidebar_posts"] = Post.objects.all().order_by("-last_modified")
+        context["sidebar_posts"] = Post.objects.all().order_by("last_modified")
         context["title"] = "Home"
         return context
 
@@ -30,11 +26,34 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
 
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        context = super().get_context_data(**kwargs)
-        context["sidebar_posts"] = Post.objects.all().order_by("-last_modified")
-        context["title"] = "Home"
-        return context
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = "blog/post_form.html"
+    fields = ["title", "content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    template_name = "blog/post_form.html"
+    fields = ["title", "content"]
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+    def handle_no_permission(self):
+        return redirect("blog-home")
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
 
 
 def about(request):
