9e2993573c3c2286db55a8337907a32df4cceb87
bhattpawan <bhattpawan1997@gmail.com>
2023-10-16 09:13:44 +0530

    Delete template for posts

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("post/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
     path("post/new/", views.PostCreateView.as_view(), name="post-create"),
     path("post/update/<int:pk>/", views.PostUpdateView.as_view(), name="post-update"),
+    path("post/delete/<int:pk>/", views.PostDeleteView.as_view(), name="post-delete"),
     path("about/", views.about, name="blog-about"),
 ]
