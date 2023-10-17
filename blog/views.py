5f51bc1219e47ef420bff8a683bd3548fe896a94
bhattpawan <bhattpawan1997@gmail.com>
2023-10-17 09:10:35 +0530

    Pagination

--- blog/views.py
+++ blog/views.py
@@ -15,10 +15,11 @@ class PostListView(ListView):
     template_name = "blog/home.html"
     context_object_name = "posts"
     ordering = ["-date_posted"]
+    paginate_by = 2
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["sidebar_posts"] = Post.objects.all().order_by("last_modified")
+        context["sidebar_posts"] = Post.objects.all().order_by("last_modified")[:5]
         context["title"] = "Home"
         return context
 
